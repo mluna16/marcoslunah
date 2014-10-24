9fb11d6e0b85baaac6000d7cc3c072517f8d7747
marcoslunah <mlunag16@gmail.com>
2014-10-24 00:58:27 -0430

    cover terminado :D

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -19,14 +19,58 @@
           <div class="inner cover">
             <h1 class="cover-heading">Marcos Luna H</h1>
             <p class="lead">Developer -  Photographer A.</p>
-            <p class="lead">
-              
-            </p>
+           <div class="row">
+             <div class="col-md-4 center-block">
+             </div>
+             <div class="col-md-4 center-block">
+                <div class="row" >
+                  <div class="col-md-2 center-block ">
+                    <a href="mailto:mlunag@gmail.com" class="socialIcons">
+                      <img src="assets/img/icon/iconmail.png">
+                    </a>
+                  </div>
+                  <div class="col-md-2 ">
+                    <a href="https://500px.com/mlunag16" class="socialIcons">
+                        <img src="assets/img/icon/icon500px.png">
+                    </a>                
+                  </div>
+                  <div class="col-md-2 ">
+                    <a href="https://facebook.com/marcoslunah" class="socialIcons">
+                        <img src="assets/img/icon/iconfacebook.png">
+                    </a>
+                  </div>
+                  <div class="col-md-2 ">
+                    <a href="https://twitter.com/marcoslunah" class="icon500px socialIcons">
+                        <img src="assets/img/icon/icontwitter.png">
+                    </a>
+                  </div>
+                  <div class="col-md-2 ">
+                    <a href="https://instagram.com/marcoslunah" class="icon500px socialIcons">
+                        <img src="assets/img/icon/iconinstagram.png">
+                    </a>
+                  </div>
+                  <div class="col-md-2 ">
+                    <a href="https://github.com/mluna16" class="icon500px socialIcons">
+                        <img src="assets/img/icon/icongithub.png">
+                    </a>
+                  </div>
+                </div>
+                <div class="col-md-4 center-block">
+                </div>
+             </div>
+           </div>
+            
+          
+            
           </div>
 
           <div class="mastfoot">
             <div class="inner">
-             
+              <p>
+                Designed with <a href="http://getbootstrap.com/">Boostrap</a> developed with <a href="http://laravel.com/">Laravel</a> powered by <a href="https://www.heroku.com/home">Heroku</a>
+              </p>
+            </div>
+              
             </div>
           </div>
 
